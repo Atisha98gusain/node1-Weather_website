bab9687260dce437715eb735fc9f3ad3210d7e4a
Atisha <mine.ishan@gmail.com>
2019-11-22 15:17:43 +0530

    add high or low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,8 @@ request({url , json: true}, (error, {body}) =>{
     } else if(body.error){
         callback('Unable to find location', undefined)
     } else{
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature  + ' temperature out there. There is a ' + body.currently.precipProbability + ' chances of rain' )
+        
+        callback(undefined,  body.daily.data[0].summary + ' It is currently ' + body.currently.temperature   + ' degrees out there. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + ' There is a ' + body.currently.precipProbability + ' chances of rain.' )
 
     }
 
